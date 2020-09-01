2145ab4906df10152c21d508cfcc820d59d40395
Eduardo Tirado <et@balmes92.com>
2020-09-01 07:57:42 +0200

    ajuste de modulo b92_crm_lead

--- balmes92/b92_crm_lead/models/crm_lead.py
+++ balmes92/b92_crm_lead/models/crm_lead.py
@@ -7,4 +7,4 @@ class CrmLead(models.Model):
     _inherit = "crm.lead"
 
     blm_documento = fields.Char(string='Enlace a Documento')
-    code = fields.Char(string='Lead Number' default="/")
+    code = fields.Char(string='Lead Number', default="/")
